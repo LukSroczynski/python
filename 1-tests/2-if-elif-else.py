23c15e47d3117965ce8915a2e4711376121bc6ed
Surreallistic <iluzion5@o2.pl>
2017-05-20 00:04:38 +0200

    test common stuff

--- 1-tests/2-if-elif-else.py
+++ 1-tests/2-if-elif-else.py
@@ -1,6 +1,10 @@
-age = 24;
+age = 1;
 
-if age > 18:
-    print("You can't buy a beer!")
+if age > 18 and age < 100:
+    print("Here you have a beer! ENJOY!");
+elif age > 100:
+    print("You are too old! Don't drink!");
 else:
-    print("Here you have a beer! ENJOY!")
+    print("You can't buy a beer!");
+
+
